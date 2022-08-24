ff0e39869e5b2485b9986c3fd5cbc3c0c614490c
uzair011 <8407uzair@gmail.com>
2022-08-24 22:03:15 +0530

    third commit

--- App.js
+++ App.js
@@ -14,23 +14,22 @@ import {
   Platform,
   Dimensions,
 } from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import WelcomeScreen from "./app/Screens/WelcomeScreen";
+import ViewImageScreen from "./app/Screens/ViewImageScreen";
 
 export default function App() {
-  const handlePress = () => console.log("text pressed.");
-  console.log(Dimensions.get("screen"));
-  return (
-    <SafeAreaView style={containerStyles}>
-      <Button
-        title="click me"
-        onPress={() => {
-          Alert.alert("MyTitle", "The Message...", [
-            { text: "OK", onPress: () => console.log("You clicked yes") },
-            { text: "Nope", onPress: () => console.log("You clicked No") },
-          ]);
-        }}
-      />
-    </SafeAreaView>
-  );
+  //const handlePress = () => console.log("text pressed.");
+  //console.log(Dimensions.get("screen"));
+  //console.log(useDeviceOrientation());
+
+  const { landscape } = useDeviceOrientation();
+
+  //return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
 
 const containerStyles = { backgroundColor: "pink" };
@@ -38,8 +37,8 @@ const containerStyles = { backgroundColor: "pink" };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightblue",
-    justifyContent: "center",
+    //backgroundColor: "lightblue",
+    //justifyContent: "center",
     alignItems: "center",
     //paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
